af8f5f75d63b101940cac6032cae7f61e7af0f8c
Vüsal Guseynov <thedragonspb@gmail.com>
2020-05-06 23:06:13 +0300

    Update AndroidUtilLogWatcher.java

--- AndroidUtilLogWatcher.java
+++ AndroidUtilLogWatcher.java
@@ -9,61 +9,13 @@ import java.util.Locale;
 /*
  *    Need add 'android.permission.READ_LOGS' in manifest
  */
-public class AndroidUtilLogWatcher extends Thread {
+public class AndroidUtilLogWatcher {
 
     private boolean watching = false;
 
     private final SimpleDateFormat logTimeFormat = new SimpleDateFormat("MM-dd hh:mm:ss.SS", Locale.UK);
 
     private LogWatcherListener logWatcherListener;
-
-    @Override
-    public void run() {
-        super.run();
-
-        Process logcat;
-        BufferedReader br;
-
-        List<String> logsBuffer = new LinkedList<>();
-
-        String currentLog;
-        Date previousLogTime = null;
-
-        try {
-            while (watching) {
-                logcat = Runtime.getRuntime().exec(new String[]{"logcat", "-d"});
-                br = new BufferedReader(new InputStreamReader(logcat.getInputStream()));
-                br.readLine();
-                while ((currentLog = br.readLine()) != null) {
-                    Date currentLogTime = logTimeFormat.parse(currentLog.substring(0, 18));
-                    if (previousLogTime != null) {
-                        if (currentLogTime.after(previousLogTime)
-                        || currentLogTime.equals(previousLogTime)
-                        ) {
-                            if (!logsBuffer.contains(currentLog)) {
-                                logsBuffer.add(currentLog);                                
-                                logWatcherListener.onListen(currentLog);
-                            }
-                            
-                            if (logsBuffer.size() > 1000) {
-                                logsBuffer.remove(0);
-                            }
-                            
-                            previousLogTime = currentLogTime;
-                        }
-                    } else {
-                        logWatcherListener.onListen(currentLog);
-                        previousLogTime = currentLogTime;
-                    }
-                }
-                logsBuffer.clear();
-                br.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            stopWatching();
-        }
-    }
 
     public AndroidUtilLogWatcher setAndroidUtilLogWatcherListener(LogWatcherListener logWatcherListener) {
         this.logWatcherListener = logWatcherListener;
@@ -73,7 +25,51 @@ public class AndroidUtilLogWatcher extends Thread {
     public void startWatching() {
         if (logWatcherListener != null) {
             watching = true;
-            start();
+            new Thread(() -> {
+                Process logcat;
+                BufferedReader br;
+
+                List<String> logsBuffer = new LinkedList<>();
+
+                String currentLog;
+                Date previousLogTime = null;
+
+                try {
+                    while (watching) {
+                        logcat = Runtime.getRuntime().exec(new String[]{"logcat", "-d"});
+                        br = new BufferedReader(new InputStreamReader(logcat.getInputStream()));
+                        br.readLine();
+                        while ((currentLog = br.readLine()) != null) {
+                            Date currentLogTime = logTimeFormat.parse(currentLog.substring(0, 18));
+                            if (previousLogTime != null) {
+                                if (currentLogTime.after(previousLogTime)
+                                || currentLogTime.equals(previousLogTime)
+                                ) {
+                                    if (!logsBuffer.contains(currentLog)) {
+                                        logsBuffer.add(currentLog);                                
+                                        logWatcherListener.onListen(currentLog);
+                                    }
+
+                                    if (logsBuffer.size() > 1000) {
+                                        logsBuffer.remove(0);
+                                    }
+
+                                    previousLogTime = currentLogTime;
+                                }
+                            } else {
+                                logsBuffer.add(currentLog);                                
+                                logWatcherListener.onListen(currentLog);
+                                previousLogTime = currentLogTime;
+                            }
+                        }
+                        logsBuffer.clear();
+                        br.close();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    stopWatching();
+                }
+            }).start();
         }
     }
 
